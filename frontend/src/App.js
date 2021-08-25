704b5e661199bb767f7fafd229b8b312e0497326
Jordi <jordi8101@gmail.com>
2021-08-25 16:46:33 -0700

    Hash router added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle, defaultTheme } from "./utils";
 import "bootstrap/dist/css/bootstrap.min.css";
